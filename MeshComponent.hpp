e7a66733ecc5df82a5b357fe67f35248f37dbb3d
Gleb Guschin <hotdraw@mail.ru>
2016-09-21 18:53:26 +0400

    1
    1

---
+++ MeshComponent.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#ifndef __MESH_COMPONENT_HPP__
+#define __MESH_COMPONENT_HPP__
+
+#include "BaseComponent.hpp"
+#include "RenderScene.hpp"
+
+class MeshComponent : public BaseComponent {
+
+	SharedPtr<MeshRenderSceneObject>   meshRenderSceneObject;
+
+public:
+
+	MeshComponent() : BaseComponent() {}
+	~MeshComponent() {}
+
+	virtual bool setMesh(const AssetName&) { return false; }
+	void update(const TimeUpdate&) {}
+
+protected:
+
+};
+
+
+#endif
